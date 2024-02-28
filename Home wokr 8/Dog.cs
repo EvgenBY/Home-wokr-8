fd660348f5811031f6ea9c275c74c1162c7d4dd4
Eugene <71292852+EvgenBY@users.noreply.github.com>
2024-02-28 16:35:11 +0300

    Add project files.

---
+++ Home wokr 8/Dog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_wokr_8
+{
+    public class Dog : Animal
+    {
+
+        // Реализация абстрактного метода Eat
+        public override void Eat()
+        {
+            Console.WriteLine("Dog is eating.");
+        }
+    }
+}
---
+++ Home wokr 8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace Home_wokr_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+
+            // Спросим у пользователя имя собаки
+            Console.WriteLine("Введите имя собаки: ");
+            string dogName = Console.ReadLine();
+
+
+            // Создаем объект типа Dog
+            Dog dog = new Dog();
+
+
+            //Устанавливаем имя и выводим его
+            dog.SetName(dogName);
+            Console.WriteLine($"Имя собаки: {dog.GetName()}");
+
+            //Вызываем метод Eat
+            dog.Eat();
+        }
+    }
+}
